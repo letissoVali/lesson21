142a9be1e97f3b273d8d26819436c896cb424e6b
Iskander <valiullin00@mail.ru>
2018-10-06 09:38:18 +0300

    commit task21

---
+++ src/ru/valiullin/ToLeft.java
@@ -0,0 +1,86 @@
+package ru.valiullin;
+
+import java.util.Random;
+
+public class ToLeft {
+
+    private static int[][] arrSort;
+    private static int sizeToLeft;
+
+    private static void setArrSort(int[][] arrSort) {
+        ToLeft.arrSort = arrSort;
+    }
+    /**
+     * util method to generate random int value
+     * @param between - take int value
+     * @return - return random int value between 0 (inclusive) - between
+     */
+    private static int random(int between) {
+        Random random = new Random();
+        return random.nextInt(between);
+    }
+    /**
+     * util method, fill elements in created array
+     *
+     */
+    private static void createArray(int size) {
+
+        setArrSort(new int[size][size]);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                arrSort[i][j] = random(10);
+            }
+        }
+    }
+
+    //TODO Task 1
+
+    public static void toLeft() {
+        sizeToLeft = 10;
+        createArray(sizeToLeft);
+        //todo 1.Пройти с 1-ой до последней строки
+        System.out.println("====================");
+        System.out.println("с 1-й до предпоследней строки ");
+        System.out.println("====================");
+        for (int i = 0; i < sizeToLeft; i++) {          // Цикл по первой размерности выводит строки
+            for (int j = 0; j < sizeToLeft; j++) {      // Цикл по второй размерности выводит колонки - вывод одной строки
+                System.out.print(arrSort[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        //todo 2.Пройти с 1-го до предпоследнего элемента
+        System.out.println("====================");
+        System.out.println("с 1-го до предпоследнего элемента ");
+        System.out.println("====================");
+        for (int i = 0; i < sizeToLeft; i++) {
+            int temp;
+            for (int j = 0; j < sizeToLeft; j++) {
+
+                if(j == sizeToLeft-1){
+                    //todo 4. Последнему элементу присвоить 0
+                    arrSort[i][j] = 0;
+                } else if(j < sizeToLeft-1) {
+                    //todo 5. В текущую ячейку поместить значение следующей
+                    temp = arrSort[i][j+1];
+                    arrSort[i][j] = temp;
+                }
+                System.out.print(arrSort[i][j] + " ");
+            }
+            System.out.print("|");
+        }
+        System.out.println();
+    }
+
+    //TODO Revers
+    public static void revers() {
+        for (int i = 0; i < sizeToLeft; i++) {
+            for (int j = sizeToLeft-1; j >= 0; j--) {
+                System.out.print(arrSort[i][j] + " ");
+            }
+            System.out.print("|");
+        }
+    }
+
+}
